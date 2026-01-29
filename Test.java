47a8bf7b5151d70c43e3ca696ed23814ef4744eb
Enzo Salum <enzomsalum@gmail.com>
2026-01-28 16:08:30 -0800

    Initial Commit

--- Test.java
+++ Test.java
@@ -2,7 +2,11 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
-    
+    /*
+    git status — shows staged/uncommitted files.
+    git remote -v — shows what remote URL you’re pushing to.
+    git branch -vv — shows current branch and upstream tracking.
+    */
     
 
 }
